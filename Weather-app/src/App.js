63ee032821e73f5773158dcf9e4e381553ce50c8
Shiva Kumar <koduri.sneu@gmail.com>
2021-07-03 16:35:40 -0400

    Add Files
    Routes

---
+++ Weather-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import WeekContainer from "./WeekContainer";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <WeekContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
